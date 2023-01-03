ac033b7c862785a4a314f8edb47164ceafae2650
Stan Pers <stanislav.kad@gmail.com>
2023-01-04 00:20:24 +0100

    prepeared methods for fetching weather data

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import * as Location from 'expo-location';
+import Loading from './Loading';
+import { Alert } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const API_KEY = "5b8262412595fde76b64573b8fb46904";
+
+const getApiUrl = (latitude, longitude) => `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.API_KEY}`;
+
+export default class extends Component {
+
+  state = {
+    isLoading: true,
+  }
+
+  getLocation = async () => {
+    try { 
+      await Location.requestForegroundPermissionsAsync();
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      console.log(latitude, longitude);
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    }
+    catch (e) {
+      Alert.alert('Cannot get location data', 'Please try again')
+      console.log(e);
+    }
+    // return currentLocation;
+  }
+
+  getWeather = async (latitude, longitude) => {
+    const url = getApiUrl(latitude, longitude);
+    const { data } = await axios.get(url);
+    console.log(data);
+  } 
+
+  componentDidMount() {
+    this.getLocation(); 
+  }
+  render() {
+    const { isLoading } = this.state;
+    return (
+      isLoading ?
+        <Loading /> :
+        null
+    );
+  }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
